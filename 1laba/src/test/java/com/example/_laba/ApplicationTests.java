ee1a6b16ea5de0084f734ede09e79bce4203b251
riyqe <64642790+riyqe@users.noreply.github.com>
2025-09-26 23:33:23 +0300

    26/09 done

---
+++ 1laba/src/test/java/com/example/_laba/ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example._laba;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(com.example._laba.ApplicationTests.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "mtuci") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
