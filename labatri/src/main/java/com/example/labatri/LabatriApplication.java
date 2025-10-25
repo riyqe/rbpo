599508e8eb54acf6b7c9a3c650c277ca4e5b881d
riyqe <chanair.way@gmail.com>
2025-10-25 15:35:57 +0300

    laba3 upd до исх второй 25/10

--- labatri/src/main/java/com/example/labatri/LabatriApplication.java
+++ labatri/src/main/java/com/example/labatri/LabatriApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LabatriApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LabatriApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LabatriApplication.class, args);
+    }
 }
