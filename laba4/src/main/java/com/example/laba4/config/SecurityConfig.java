805e2049fe4b0241d95ed50354a881ded997cd6f
riyqe <chanair.way@gmail.com>
2025-11-22 15:54:34 +0300

    22/11 done 4lab

---
+++ laba4/src/main/java/com/example/laba4/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    // Бин, который будет шифровать пароли.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").hasRole("ADMIN")
+                        // Создавать/менять/удалять пользователей может только админ
+                        .requestMatchers("/api/users/**").hasRole("ADMIN")
+                        // Все остальные запросы требуют входа в систему (любая роль)
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()); // Включаем Basic Auth
+
+        return http.build();
+    }
+}
